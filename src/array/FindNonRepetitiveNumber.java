15e17d9013e824fa88b4e3b5a5f9aa349f3e8fde
HSB007 <www.hemant.bisht@oracle.com>
2018-02-11 13:33:53 +0530

    Finding one non repetitive number in the array in O(n) time using XOR operator.

---
+++ src/array/FindNonRepetitiveNumber.java
@@ -0,0 +1,25 @@
+package array;
+
+//@Author : Hemant Singh Bisht
+//@Note : This program is only to find 1 non repetitive number in the Array. if you pass more than
+//        one non repetitive number in the array it will fail.
+
+public class FindNonRepetitiveNumber {
+
+	public static int findNonRepetitiveNum(int inputArr[])
+	{
+		int result =0;
+		for(int index : inputArr)
+			result ^= index;
+		
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		
+		int []input = {4,2,1,2,4,6,7,1,7};
+		int number = FindNonRepetitiveNumber.findNonRepetitiveNum(input);
+		System.out.println("Non Repititive Number is : " + number);
+	}
+
+}
